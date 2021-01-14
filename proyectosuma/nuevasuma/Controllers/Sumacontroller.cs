486d59e3d4665d421b3e8c8529ce37c525ab41dd
Eduardo Sossa <Eduardo Sossa@DESKTOP-812FA3V>
2021-01-13 20:41:23 -0400

    suma con get

---
+++ proyectosuma/nuevasuma/Controllers/Sumacontroller.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace nuevasuma.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Sumacontroller : Controller
+    {
+        [HttpGet]
+        public int suma(int num1, int num2)
+        {
+            return num1 + num2; 
+        }
+    }
+}
